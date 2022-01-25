2f47eb29900f2e038e80330e1b0d3820f6ea2e00
Yash Patel <62371168+yashpatel4900@users.noreply.github.com>
2022-01-25 22:12:39 +0530

    Deperating and Creating Date Component of ExpenseItem

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,4 +1,5 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
   // This should be done in App.js file so that it changes dynamically and
@@ -10,9 +11,22 @@ function ExpenseItem(props) {
   // const expenseTitle = "Car Insurance";
   // const expenseAmount = 294.67;
 
+  //   This was written here befor but now movinf it to new ExpenseDate.js
+  //   const month = props.date.toLocaleString("en-US", { month: "long" });
+  //   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  //   const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      {/* This was written here before but now moving it to ExpenseDate.js */}
+      {/* <div>
+        This is also Valid
+        // <div>{props.date.toLocaleString("en-US", { month: "long" })}</div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div> */}
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
