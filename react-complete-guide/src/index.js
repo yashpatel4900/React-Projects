9cd2d9101bb8790d82294420e7bcc6bfc4c879ff
Yash Patel <62371168+yashpatel4900@users.noreply.github.com>
2022-01-25 20:22:25 +0530

    Creating Components and exporting to use in App.js

--- react-complete-guide/src/index.js
+++ react-complete-guide/src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+// React DOM is used to call its method render which requires two arguments to be passed
+// Second arguments points to ID of index.html file of public folder which will be replaced by <App />
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
