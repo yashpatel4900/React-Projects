e4d7be1af90b497f2a287d048cc8470177b0d18f
Yash Patel <62371168+yashpatel4900@users.noreply.github.com>
2022-01-25 21:45:06 +0530

    Adding Styling

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -11,11 +11,11 @@ function ExpenseItem(props) {
   // const expenseAmount = 294.67;
 
   return (
-    <div>
+    <div className="expense-item">
       <div>{props.date.toISOString()}</div>
-      <div>
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div>{props.amount}</div>
+        <div className="expense-item__price">$ {props.amount}</div>
       </div>
     </div>
   );
