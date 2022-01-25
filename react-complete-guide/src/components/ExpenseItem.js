4b63357fcfd6b723d6f7b6b5da03f5cd17e9c533
Yash Patel <62371168+yashpatel4900@users.noreply.github.com>
2022-01-25 21:11:05 +0530

    Setting data change dynamically using props

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,10 +1,21 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  // This should be done in App.js file so that it changes dynamically and
+  // whole ExpenseItem Component can be reused.
+
+  // These constants are declared in JS so that then it can be used inside
+  // HTML code to make it change dynamically
+  // const expenseDate = new Date(2021, 2, 28);
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 294.67;
+
   return (
     <div>
-      <div>March 28th 2021</div>
+      <div>{props.date.toISOString()}</div>
       <div>
-        <h2>Car Insurance</h2>
-        <div>$294.67</div>
+        <h2>{props.title}</h2>
+        <div>{props.amount}</div>
       </div>
     </div>
   );
